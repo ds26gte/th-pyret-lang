63d45e99f1f82ace9c0dd0ef8e6c758bce47384e
Dorai Sitaram <ds26gte@gmail.com>
2025-03-02 12:23:44 -0500

    ...

--- ide/webpack.config.js
+++ ide/webpack.config.js
@@ -22,8 +22,10 @@ const config = {
         filename: 'static/js/bundle[name].js',
         // There are also additional JS chunk files if you use code splitting.
         chunkFilename: 'static/js/[name].chunk.js',
+        publicPath: '/',
     },
     devServer: {
+      // static: './build',
         open: true,
         host: 'localhost',
     },
