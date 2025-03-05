5b628e6e1b2e359f8ca2d400db39264f0b981138
Dorai Sitaram <ds26gte@gmail.com>
2025-03-05 17:51:55 -0500

    ...

--- ide/server.js
+++ ide/server.js
@@ -1,22 +1,10 @@
 const express = require('express');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
 const app = express();
-const configFun = require('./webpack.config.js');
-const config = configFun();
-console.log('config = ', config);
-const compiler = webpack(config);
-
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: config.output.publicPath,
-  })
-);
+app.use(express.static(__dirname + "/build/"));
 
 // Serve the files on port 3000.
 app.listen(8081, function () {
   console.log('Example app listening on port 8081!\n');
 });
+
+
